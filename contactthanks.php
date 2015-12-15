c565c03c5bfb7d17bc9f45cb7d5a4af83b0e3214
Courtney513 <ck521709@E224L63.local>
2015-12-15 15:56:43 -0600

    Final push before website goes live
    website is now live

---
+++ contactthanks.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>Contact Thanks page</title>
+<link href="https://fonts.googleapis.com/css?family=Lato:400,700,900" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="css/normalize.css" />
+<link rel="stylesheet" href="css/foundation.css" />
+<link rel="stylesheet" href="css/custom.css" />
+<style>
+
+</style>
+</head>
+<body class="center-thank-you-message">
+
+<div>
+  <h1>Your message has been sent!</h1>
+  <br />
+  <p><a class="thank-you-message" href="index.html">go back!</a></p>
+</div>
+<!--<footer>
+  <div class="row footer-contact-info">
+    <div class="row">
+      <div class="columns">
+        <h4 class="right">Contact Jason Today</h4>
+      </div>
+    </div>
+    <div class="row">
+      <div class="columns footer-phone-number"> <a href="tel:+14024990063" target="_blank" class="right">+1-402-499-0063</a> </div>
+    </div>
+    <div class="row">
+      <div class="columns footer-email"> <a href="mailto:theonlineformula@gmail.com" class="right">theonlineformula@gmail.com</a> </div>
+    </div>
+    <div class="row">
+      <div class="small-12 columns"> <small class="copywrite right">Jason Pearson Diet © 2015</small> </div>
+    </div>
+  </div>
+</footer>-->
+<!--<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+	</script> 
+<script type="text/javascript">
+	_uacct = "UA-68528-29";
+	urchinTracker();
+	</script>-->
+</body>
+</html>
